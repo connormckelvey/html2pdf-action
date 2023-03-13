279fb41164b55d8a7090b8ea16ab91dfd5c40542
Connor McKelvey <connormckelvey@gmail.com>
2023-03-13 06:18:26 -0600

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,6 @@
 
 
 main() {
-    echo files in cwd
-    ls -la
-    echo file in /github/workspace
-    ls -la /github/workspace
-    echo pwd
-    pwd
-
-    echo "/github"
-    find "/github"
-    env
-    sleep 5
-
     html2pdf "$@"
 }
 
